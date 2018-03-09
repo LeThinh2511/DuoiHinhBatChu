95bf1410f4a578d911cd66999b95426900a9a70f
LeThinh2511 <thinhlefrs@gmail.com>
2018-03-09 18:50:07 +0700

    -Update Question class -Add updateUI() function -create point and round variable

--- DuoiHinhBatChu/DuoiHinhBatChu/Question.swift
+++ DuoiHinhBatChu/DuoiHinhBatChu/Question.swift
@@ -8,30 +8,39 @@
 
 import Foundation
 
-class Question
+class Question: Hashable, Equatable
 {
+    var hashValue: Int
+    {
+        return answear.hashValue
+    }
+    
+    static func ==(lhs: Question, rhs: Question) -> Bool {
+        return lhs.answear == rhs.answear
+    }
+    
     var image: String
     var answear: String = ""
-    
-
-    var hint: Set<Character> = []
+    var hint: Dictionary<Int, Character> = [:]
     
     init(answear: String, image: String) {
         self.answear = answear
         self.image = image
         
         /* add all characters to the "hint" Set from "answear" */
+        var count: Int = 0
         for i in self.answear
         {
-            self.hint.insert(i)
-            print(self.hint)
+            hint[count] = i
+            count = count + 1
         }
         // Adding more character to the "hint" property
-        let numOfChar: Int = self.answear.count/2 + 1
+        let numOfChar: Int = self.answear.count/2
         
         for _ in 0..<numOfChar
         {
-            self.hint.insert(Character.getARandomLetter())
+            hint[count] = Character.getARandomLetter()
+            count = count + 1
         }
     }
 }
--- DuoiHinhBatChu/DuoiHinhBatChu/ViewController.swift
+++ DuoiHinhBatChu/DuoiHinhBatChu/ViewController.swift
@@ -13,11 +13,21 @@ class ViewController: UIViewController {
 
     @IBOutlet var roundLabel: UILabel!
     @IBOutlet var PointLabel: UILabel!
+    @IBOutlet var imageQuestion: UIImageView!
+    
+    @IBOutlet weak var answerButton: UIView!
+    @IBOutlet weak var hintButton: UIView!
+    var round: Int! = 0
+    var point: Int! = 0
+    let setQuestion: [Question] = [
+        Question(answear: "AIMO", image: "aiMo"),
+        Question(answear: "TAMTHAT", image: "tamThat")
+    ]
+    
     
     @IBAction func reset(_ sender: UIButton)
     {
         print("reset tapped")
-        roundLabel.text = "round 12"
     }
     
     @IBAction func submit(_ sender: UIButton)
@@ -27,13 +37,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI(round: 0)
     }
-
+    
+    func updateUI(round: Int)
+    {
+        roundLabel.text = "Round \(round)"
+        PointLabel.text = "\(point!)"
+        imageQuestion.image = UIImage(named: setQuestion[round].answear)
+        
+        for i in 0..<setQuestion.count
+        {
+            print(i)
+            let button: UIButton = UIButton()
+            button.setTitle("T", for: UIControlState.normal)
+            button.backgroundColor = UIColor.green
+            self.answerButton.addSubview(button)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
